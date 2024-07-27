26cecf522f9ebbe88e61ccd1f7c9e133ec49449d
krishna upadhyay <kupadhyay2394@gmail.com>
2024-07-27 23:41:11 +0530

    add util

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class Apiesponse{
+    constructor(statusCode,data,message="Success"){
+        this.statusCode=statusCode
+        this.data=data
+        this.message=message
+        this.success=statusCode<400
+    }
+}
+export {Apiesponse}
